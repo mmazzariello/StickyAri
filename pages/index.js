c08b68978526593c45c43dda9e2ffd1dd91e270e
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-04-18 23:21:18 +0200

    first version with complete CRUD

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,133 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Textarea, Box, Button, Input, IconButton } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
+import * as React from "react";
+import { HiOutlineTrash } from "react-icons/hi";
 
 export default function Home() {
+  const [notes, setNotes] = React.useState(
+    JSON.parse(window.localStorage.getItem("notesEnLS") || "{}")
+  );
+
+  const handleAddNote = () => {
+    const newNote = { id: uuidv4(), text: "" };
+    const updatedListOfNotes = { ...notes, [newNote.id]: newNote };
+    setNotes(updatedListOfNotes);
+    window.localStorage.setItem(
+      "notesEnLS",
+      JSON.stringify(updatedListOfNotes)
+    );
+  };
+
+  const handleUpdateNote = (id, text) => {
+    const updatedList = { ...notes, [id]: { id: id, text: text } };
+    setNotes(updatedList);
+    window.localStorage.setItem("notesEnLS", JSON.stringify(updatedList));
+  };
+
+  const handleDeleteNote = (id) => {
+    const updatedList = { ...notes };
+    delete updatedList[id];
+    setNotes(updatedList);
+    window.localStorage.setItem("notesEnLS", JSON.stringify(updatedList));
+  };
+
+  // let notes {
+  //   "123": {
+  //     id: "123",
+  //     text: "Hola"
+  //   },
+  //   "333": {
+  //     id: "333",
+  //     text: "nota 2"
+  //   }
+  // }
+
+  // const handleDelete = (title) => {
+  //   const newPosts = posts.filter((item) => item.title !== title);
+  //   setPosts(newPosts);
+  //   window.localStorage.setItem("postsEnLS", JSON.stringify(newPosts));
+  // };
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Box>
+          <Button onClick={handleAddNote}>+</Button>
+        </Box>
+        <AllNotes
+          notes={notes}
+          onUpdateNote={handleUpdateNote}
+          onDelete={handleDeleteNote}
+        />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
+
+const AllNotes = ({ notes, onUpdateNote, onDelete }) => {
+  return (
+    <Box display="flex">
+      {Object.values(notes).map((note) => {
+        console.log(note.id);
+        return (
+          <Note
+            key={note.id}
+            note={note}
+            onUpdateNote={onUpdateNote}
+            onDelete={onDelete}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+const Note = ({ note, onUpdateNote, onDelete }) => {
+  // const [initialText, setInitialText] = React.useState(note.text);
+  const [text, setText] = React.useState(note.text);
+
+  return (
+    <Box boxShadow="md" minWidth="100px" minHeight="100px" padding="4px">
+      <Textarea
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+          onUpdateNote(note.id, text);
+        }}
+      />
+      <IconButton
+        icon={<HiOutlineTrash />}
+        variant="ghost"
+        size="sm"
+        colorScheme="red"
+        onClick={() => onDelete(note.id)}
+      />
+    </Box>
+  );
+};
+
+const AllPosts = ({ onDelete, onEdit }) => {
+  const posts = JSON.parse(window.localStorage.getItem("postsEnLS") || "[]");
+
+  return (
+    <Box>
+      {posts.map((post, index) => {
+        return (
+          <Box key={index}>
+            {post.title}
+            {post.content}
+
+            <Button onClick={() => onDelete(post.title)}>Delete</Button>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+};
+
+const EditPost = ({ onEdit }) => {};
