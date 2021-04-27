7f501a0e8e56606cdf7606e3b3f4d94b0d87bbb5
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-04-27 21:34:47 +0200

    bug in setText resolved

--- src/Home.js
+++ src/Home.js
@@ -140,6 +140,7 @@ const AllNotes = ({ notes, onUpdateNote, onDelete, pageConstraintsRef }) => {
 const Note = ({ note, onUpdateNote, onDelete, pageConstraintsRef }) => {
   // const [initialText, setInitialText] = React.useState(note.text);
   const [text, setText] = React.useState(note.text);
+  console.log("ok", text);
   return (
     <motion.div
       onDragEnd={(event, info) => {
@@ -176,8 +177,11 @@ const Note = ({ note, onUpdateNote, onDelete, pageConstraintsRef }) => {
           border="none"
           resize="none"
           onChange={(e) => {
-            setText(e.target.value);
-            onUpdateNote(note.id, text);
+            const newText = e.target.value;
+            setText(newText);
+            onUpdateNote(note.id, newText);
+            console.log("una letra menos", text);
+            console.log("newText", newText);
           }}
         />
         <Box display="flex" justifyContent="flex-end">
